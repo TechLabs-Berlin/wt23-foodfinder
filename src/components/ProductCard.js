059432ea7906913e93dcadc2f1e728d188e80d7d
lurelureg <lucreciagarino@gmail.com>
2023-02-25 11:03:44 +0100

    working ProductSearch.js and ProductCard.js added to App

---
+++ src/components/ProductCard.js
@@ -0,0 +1,6 @@
+function ProductCard({ product }) {
+    // contains: img, name, brand, amount, icons for intolerances <= 4
+    return <div>Im a Product {product._id}</div>;
+}
+
+export default ProductCard;
