6e7315a6562f5c8dd2e585ac3fcbc19d718a50c0
lurelureg <lucreciagarino@gmail.com>
2023-03-03 13:09:08 +0100

    added warning, working product labels

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,7 +1,26 @@
 import { IonIcon, IonItem, IonLabel, IonButton } from "@ionic/react";
 import { star } from "ionicons/icons";
+import { Icon } from "@iconify/react";
 
 function ProductItem({ product }) {
+    // console.log(product);
+    const glutenFree = product.labels_tags.includes("en:no-gluten") ? (
+        <Icon icon="mdi:gluten-free" color="#D65B79" />
+    ) : null;
+    const gluten = !product.labels_tags.includes("en:no-gluten") ? (
+        <Icon icon="mdi:gluten" color="#5bd6b7" />
+    ) : null;
+    const vegetarian = product.labels_tags.includes("en:vegetarian") ? (
+        <Icon icon="lucide:leaf" color="#5bd6b7" />
+    ) : null;
+    const vegan = product.labels_tags.includes("en:vegan") ? (
+        <Icon icon="lucide:vegan" color="#5bd6b7" />
+    ) : null;
+    const allergen =
+        product.allergens.length > 0 ? (
+            <Icon icon="lucide:info" color="#D65B79" />
+        ) : null;
+
     // contains: img, name, brand, amount, icons for intolerances <= 4
     return (
         // detail: chevron button
@@ -9,7 +28,7 @@ function ProductItem({ product }) {
             <IonLabel>
                 <h3>{product.product_name}</h3>
                 <p>Brand:{product.brands}</p>
-                <p>Qty:{product.quantity}</p>
+                {/* <p>Qty:{product.quantity}</p> */}
             </IonLabel>
             {/* end star for favorite/navigate for navigation */}
             <IonButton
@@ -29,6 +48,11 @@ function ProductItem({ product }) {
                     src={product.image_front_small_url}
                 />
             </ion-thumbnail>
+            {gluten}
+            {glutenFree}
+            {vegetarian}
+            {vegan}
+            {allergen}
         </IonItem>
     );
 }
