e955c5a28f353af6d8d3a0381479da9fa06839b5
lurelureg <lucreciagarino@gmail.com>
2023-03-10 19:39:15 +0100

    Rendering favorites in tab 3

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,28 +1,53 @@
+import useMyProductsContext from "../hooks/use-products-context";
+import ProductItem from "../components/ProductItem";
+import { useState } from "react";
+
 import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
+    IonButton,
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
 } from "@ionic/react";
 
 const Profile = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Profile</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-      </IonContent>
-    </IonPage>
-  );
+    const { favorites } = useMyProductsContext();
+    let [favs, setFavs] = useState([]);
+
+    const handleClick = (event) => {
+        event.preventDefault();
+
+        const favList = favorites.map((product, index) => {
+            return (
+                <ProductItem
+                    product={product}
+                    key={index}
+                    // onClick={handleProductClick}
+                />
+            );
+        });
+        setFavs(favList);
+    };
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>My Food Finder</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">My Food Finder</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonButton onclick={handleClick}>Favorites</IonButton>
+                {favs}
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Profile;
