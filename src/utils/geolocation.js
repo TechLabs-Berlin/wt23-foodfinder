f88b98da8c896a36de85bd1e418935955957534a
lurelureg <106522330+lurelureg@users.noreply.github.com>
2023-03-11 12:34:27 +0100

    disable imports

--- src/utils/geolocation.js
+++ src/utils/geolocation.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 function applyDistanceFormula(startLat, startLng, endLat, endLng) {
   const R = 6371; // radius Earth
