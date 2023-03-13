2d962b8ce4fc13637ed0402c492b7e6fdc467db9
lurelureg <lucreciagarino@gmail.com>
2023-03-13 15:49:58 +0100

    setting capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'wt23.foodfinder.ionic.react.app',
+  appName: 'wt23-foodfinder-ionic-react-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
